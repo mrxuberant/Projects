ba8f39d91ed20d07fcef7bc5fcf7f6d3ecee76e5
mrxuberant <55145635+mrxuberant@users.noreply.github.com>
2021-08-18 17:37:07 +0530

    Add files via upload

---
+++ TheSparksFoundation/users.js
@@ -0,0 +1,21 @@
+// const mongoose = require('mongoose')
+
+
+
+// mongoose.connect('mongodb://localhost:27017/banking_app')
+
+// const User = new Schema(
+//     {
+//         customer: {
+//             type: String
+//         },
+//         current_balance: {
+//             type: Int
+//         }
+//     }
+// )
+
+
+
+
+// module.exports = mongoose.Model('User', user);
